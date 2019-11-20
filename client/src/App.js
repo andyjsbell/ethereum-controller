310914e02c500b00e53b08740d0ae1db0f29026e
Andy Bell <andy.bell@displaynote.com>
2019-11-20 16:04:40 +0100

    make web3

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, useState } from 'react';
 import './App.css';
+import getWeb3 from "./utils/getWeb3";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Web3 = require('web3');
+
+class App extends Component {
+
+  state = {web3: null, accounts: []};
+  
+  handleChange(event, newValue) {
+    this.setState({value: newValue});
+  }
+
+  componentDidMount = async () => {
+    try {
+      // Workaround for compatibility between web3 and truffle-contract
+      
+      Web3.providers.HttpProvider.prototype.sendAsync = Web3.providers.HttpProvider.prototype.send;	
+      
+      const web3 = await getWeb3();
+      const accounts = await web3.eth.getAccounts();
+      this.setState({ web3:web3, accounts:accounts });
+      
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+
+      console.error(error);
+    }
+  };
+
+  render() {
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contracts...</div>;
+    }
+    
+    return (
+      <div className="App">
+        Connected to Web3 :-)
+      </div>
+    );
+  }
 }
+
 
 export default App;
