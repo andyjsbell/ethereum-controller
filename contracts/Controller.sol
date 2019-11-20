928447fe878852fe3892aad44c14fbed3c73f92b
Andy Bell <andy.bell@displaynote.com>
2019-11-20 21:11:52 +0100

    Get public key from signature

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -13,4 +13,28 @@ contract Controller {
         theContract.paidFor();
     }
 
+    function getAddressOfSigner(bytes32 hash, bytes memory signature)
+        public
+        pure
+        returns(address) {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        require(signature.length == 65, 'Invalid length of signature');
+
+        assembly {
+            r := mload(add(signature, 0x20))
+            s := mload(add(signature, 0x40))
+            v := byte(0, mload(add(signature, 0x60)))
+        }
+
+        require(v > 1, 'Invalid v');
+
+        if (v < 27) {
+            v += 27;
+        }
+
+        return ecrecover(hash, v, r, s);
+    }
 }
