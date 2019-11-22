57d5952b13b6ddb17f379dc5ae0ccb8d3e184e05
Andy Bell <andy.bell@displaynote.com>
2019-11-22 12:10:03 +0100

    Send transaction with local identity to ethreum blockchain, I am pretty sure this won't work but hey

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,28 @@ const Home = (props) => {
   const [hashedMessage, setHashedMessage] = useState('');
   const [accountFromSignature, setAccountFromSignature] = useState('');
 
+  const sendTransaction = () => {
+    var rawTx = {
+      nonce: '0x00',
+      gasPrice: '0x09184e72a000', 
+      gasLimit: '0x2710',
+      to: '0x0000000000000000000000000000000000000000', 
+      value: '0x00', 
+      data: '0x7f7465737432000000000000000000000000000000000000000000000000000000600057'
+     }
+     var tx = new Tx(rawTx);
+     tx.sign(localIdentity.privateKey);
+     var serializedTx = tx.serialize();
+     //console.log(serializedTx.toString('hex'));
+     //f889808609184e72a00082271094000000000000000000000000000000000000000080a47f74657374320000000000000000000000000000000000000000000000000000006000571ca08a8bbf888cfa37bbf0bb965423625641fc956967b81d12e23709cead01446075a01ce999b56a8a88504be365442ea61239198e23d1fce7d00fcfc5cd3b44b7215f
+     web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'), function(err, hash) {
+      if (!err)
+        console.log(hash); // "0x7f9fade1c0d57a7af66ab4ead79fade1c0d57a7af66ab4ead7c2c2eb7b11a91385"
+      else 
+        console.log(err);
+    });     
+  };
+
   const signMessage = () => {
     const hash = props.web3.sha3(message)
     props.web3.eth.sign(props.accounts[0], hash, (e,r) => {
@@ -54,6 +76,7 @@ const Home = (props) => {
       <Button primary onClick={() => signMessage()}>Sign Message</Button>
       <Button secondary onClick={() => signLocalMessage()}>Sign Message Local</Button>
       <Button primary onClick={() => confirmMessage()}>Confirm Message</Button>
+      <Button secondary onClick={() => sendTransaction()}>Send Transaction</Button>
       <h3>Hash: {hashedMessage}</h3>
       <h3>Signature: {signature}</h3>
       <h3>Account from Signature: {accountFromSignature}</h3>
